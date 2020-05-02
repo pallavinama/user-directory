3da65adc04c4bf666ad01fd0d3f8d953df9ea2f5
pallavi <pallavi_nama@yahoo.com>
2020-05-02 16:01:47 -0400

    Correction of data render

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,34 +3,27 @@ import Emp from "./Emp";
 
 const List = (props) => {
   return (
-    <div className="container">
-         <div className="row">
-                <h2>You have {props.employees.length} Users.</h2>
-            </div>
-      
-      <div>
-      <table className="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Avatar</th>
-                            <th scope="col">First Name</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Phone</th>
-                            <th scope="col">Country</th>
-                            <th scope="col">Gender</th>
-                        </tr>
-                      </thead>
-                <tbody>
-                       
+    <div className="container">      
+      <div className="row">
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th scope="col">Avatar</th>
+              <th scope="col">First Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Country</th>
+              <th scope="col">Gender</th>
+            </tr>
+          </thead>
             {props.employees.map((employee) => (
-              <Emp {...employee} key={employee.id}/>
+              <tbody>
+                <Emp {...employee} key={employee.id} />
+              </tbody>
             ))}
-            
-                   </tbody>
-
-            </table>
+        </table>
       </div>
-      </div>
+    </div>
     
   );
 };
