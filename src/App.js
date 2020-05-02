cc9e6d416bbd7b013124a10995875442accde482
pallavi <pallavi_nama@yahoo.com>
2020-05-02 10:21:21 -0400

    test changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/Shared/NavBar";
+// import NavBar from "./components/Shared/NavBar";
 // import Home from "./containers/Home";
-// import EmployeeDirectory from "./containers/EmployeeDirectory";
+import EmployeeDirectory from "./containers/EmployeeDirectory";
 function App() {
   return (
     <Router>
-      <NavBar />
-      <Route exact path="/" component={Home} />
+      {/* <NavBar /> */}
+      {/* <Route exact path="/" component={Home} /> */}
       <Route exact path="/directory" component={EmployeeDirectory} />
-    </Router
-  //   <div className="App">
-  //  <h1>Welcome to user directory Page</h1>
-  //   </div>
+    </Router>
 );
 }
 
