6b62f6c34f905991a60de5fec45a17e56009e4ac
pallavi <pallavi_nama@yahoo.com>
2020-05-01 23:41:51 -0400

    Components & containers related code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/Shared/NavBar";
+// import Home from "./containers/Home";
+// import EmployeeDirectory from "./containers/EmployeeDirectory";
 function App() {
   return (
-    <div className="App">
-   <h1>Welcome to user directory Page</h1>
-    </div>
+    <Router>
+      <NavBar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/directory" component={EmployeeDirectory} />
+    </Router
+  //   <div className="App">
+  //  <h1>Welcome to user directory Page</h1>
+  //   </div>
 );
 }
 
